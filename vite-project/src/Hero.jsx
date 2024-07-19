1a607db05274b6745ca9df538d79b64b36cc7977
Alexia Christelle <christellealexia53@gmail.com>
2024-07-19 17:29:55 +0200

    Added styles to my pages

---
+++ vite-project/src/Hero.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export function Hero (){
+    return (
+        <div>
+            <img src='\src\images\Group 77.png' className='image1'/>
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+            </p>
+        </div>
+    )
+}
---
+++ vite-project/src/Navbar.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export function Navbar(){
+    return (
+        <div>
+        <img src='\src\images\airbnb1.png' className='image'/>
+       
+        </div>
+    )
+
+}
